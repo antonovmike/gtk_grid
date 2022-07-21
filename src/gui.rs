929b731753d6e0da481f891e7b6c647502e83459
Mihail Antonov <antonovma@outlook.com>
2022-07-21 19:27:03 +0400

    external elements

--- src/gui.rs
+++ src/gui.rs
@@ -76,92 +76,85 @@ pub fn build_ui(application: &gtk::Application) {
     // Create button and put it into the grid at
     // (0, 0, x, x) = (horizontal, vertical, width, height)
 
-    // --> ROW 1
+    // --> NUMERIC BUTTONS
     let button_1 = create_button("Button 1");
     let button_2 = create_button("Button 2");
     let button_3 = create_button("Button 3");
-    button_1.connect_clicked(move |_| println!("Button 1"));
-    button_2.connect_clicked(move |_| println!("Button 2"));
-    button_3.connect_clicked(move |_| println!("Button 3"));
-    grid.attach(&button_1, 0, 1, 1, 1);
-    grid.attach(&button_2, 1, 1, 1, 1);
-    grid.attach(&button_3, 2, 1, 1, 1);
-    
-    // --> ROW 2
     let button_4 = create_button("Button 4");
     let button_5 = create_button("Button 5");
     let button_6 = create_button("Button 6");
-    button_4.connect_clicked(move |_| println!("Button 4"));
-    button_5.connect_clicked(move |_| println!("Button 5"));
-    button_6.connect_clicked(move |_| println!("Button 6"));
-    grid.attach(&button_4, 0, 2, 1, 1);
-    grid.attach(&button_5, 1, 2, 1, 1);
-    grid.attach(&button_6, 2, 2, 1, 1);
-    
-    // --> ROW 3
     let button_7 = create_button("Button 7");
     let button_8 = create_button("Button 8");
     let button_9 = create_button("Button 9");
+    let button_0 = create_button("Button 0");
+
+    // --> BUTTON FUNCTION
+    button_1.connect_clicked(move |_| println!("Button 1"));
+    button_2.connect_clicked(move |_| println!("Button 2"));
+    button_3.connect_clicked(move |_| println!("Button 3"));
+    button_4.connect_clicked(move |_| println!("Button 4"));
+    button_5.connect_clicked(move |_| println!("Button 5"));
+    button_6.connect_clicked(move |_| println!("Button 6"));
     button_7.connect_clicked(move |_| println!("Button 7"));
     button_8.connect_clicked(move |_| println!("Button 8"));
     button_9.connect_clicked(move |_| println!("Button 9"));
-    grid.attach(&button_7, 0, 3, 1, 1);
-    grid.attach(&button_8, 1, 3, 1, 1);
-    grid.attach(&button_9, 2, 3, 1, 1);
-    
-    // --> ROW 5 - LABEL
-    // let counter_label = Label::new(Some("0.0"));
+    button_0.connect_clicked(move |_| {
+        let i: u8 = rand::random();
+        println!("Button 0: {}", i)
+    });
+  
+    // --> EXTRA BUTTONS
     let counter_label = create_label(Some("0.0"));
-    grid.attach(&counter_label, 0, 5, 4, 1);
+    let plus_button = create_button("+");
+    let minus_button = create_button("-");
+    let time = format!("{}", Local::now().format("%Y-%m-%d %H:%M:%S"));
+    let quit_button = create_button("Quit");
+    let label_time = create_label(None);
+    label_time.set_text(&time);
 
-    // --> ROW 4
-    let plus_button = gtk::Button::with_label("+");
-    let button_0 = create_button("Button 0");
-    let minus_button = gtk::Button::with_label("-");
+    // --> EXTRA FUNCTIONS
     plus_button.connect_clicked(glib::clone!(@weak counter_label => move |_| {
         let nb = counter_label.text()
             .parse()
             .unwrap_or(0.0);
         counter_label.set_text(&format!("{}", nb + 1.1));
     }));
-    button_0.connect_clicked(move |_| {
-        let i: u8 = rand::random();
-        println!("Button 0: {}", i)
-    });
     minus_button.connect_clicked(glib::clone!(@weak counter_label => move |_| {
         let nb = counter_label.text()
             .parse()
             .unwrap_or(0.0);
         counter_label.set_text(&format!("{}", nb - 1.2));
     }));
-    grid.attach(&plus_button, 0, 4, 1, 1);
-    grid.attach(&button_0, 1, 4, 1, 1);
-    grid.attach(&minus_button, 2, 4, 1, 1);
-
-    // --> ROW 6 - TIMER
-    let time = format!("{}", Local::now().format("%Y-%m-%d %H:%M:%S"));
-    let label_time = gtk::Label::new(None);
-    label_time.set_text(&time);
-    grid.attach(&label_time, 0, 6, 4, 1);
-    
-    // Changing time
-    let tick = move || {
-        let time = format!("{}", Local::now().format("%Y-%m-%d %H:%M:%S"));
-        label_time.set_text(&time);
-        // What is this?
-        glib::Continue(true)
-    };
-    // First digit is the rate of time update in seconds
-    glib::timeout_add_seconds_local(1, tick);
-
-    // --> ROW 2 COLUMN 4 Quit button
-    let quit_button = gtk::Button::with_label("Quit");
     quit_button.connect_clicked(clone!(@weak window => move |_| 
         unsafe {
             window.destroy()
         }
     ));
+
+    grid.attach(&button_1, 0, 1, 1, 1);
+    grid.attach(&button_2, 1, 1, 1, 1);
+    grid.attach(&button_3, 2, 1, 1, 1);
+    grid.attach(&button_4, 0, 2, 1, 1);
+    grid.attach(&button_5, 1, 2, 1, 1);
+    grid.attach(&button_6, 2, 2, 1, 1);
+    grid.attach(&button_7, 0, 3, 1, 1);
+    grid.attach(&button_8, 1, 3, 1, 1);
+    grid.attach(&button_9, 2, 3, 1, 1);
+    grid.attach(&button_0, 1, 4, 1, 1);
+    grid.attach(&counter_label, 0, 5, 4, 1);
+    grid.attach(&plus_button,   0, 4, 1, 1);
+    grid.attach(&minus_button,  2, 4, 1, 1);
+    grid.attach(&label_time,    0, 6, 4, 1); // Borrowed here
     grid.attach(&quit_button, 3, 1, 1, 4);
+
+    // Changing time
+    let tick = move || {
+        let time = format!("{}", Local::now().format("%Y-%m-%d %H:%M:%S"));
+        label_time.set_text(&time); // Moved here
+        glib::Continue(true)
+    };
+    // First digit is the rate of time update in seconds
+    glib::timeout_add_seconds_local(1, tick);
 
 // --> KEYBOARD ENDS HERE <--
 
